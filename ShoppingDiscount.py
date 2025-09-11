ba1ef2a573f50c50dd8c8a9fa9d20ffd53a5dfe8
Qbehoopin <quataziajohnson@gmail.com>
2025-09-11 23:56:46 +0000

    Add initial implementation of shopping discount program

---
+++ ShoppingDiscount.py
@@ -0,0 +1,6 @@
+# Shopping Discount Program
+price = int(input('Enter price: '))
+discount = price * 0.20
+final_price = price - discount
+print('Original Price:', price)
+print('Discounted Price:', final_price)
