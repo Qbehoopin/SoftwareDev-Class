799a4463552cced4cdfee319e8d820b4e03c995e
Qbehoopin <quataziajohnson@gmail.com>
2025-09-12 18:06:01 +0000

    Add Student Info Program to collect and display user information

---
+++ Info Program.py
@@ -0,0 +1,7 @@
+# Student Info Program
+name = input("Enter your name: ")
+sex = input("Enter your sex: ")
+school = input("Enter your school: ")
+classification = input("Enter your classification: ")
+
+print(f"My name is {name}. I am {sex}, studying at {school}, and my classification is {classification}.")
